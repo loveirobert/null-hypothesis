022113e21a86448c5d4dcecc814ac2a1c89736be
Robert Lovei <loveirobert@gmail.com>
2020-10-20 23:27:44 +0200

    better submodule export

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const {
-  getContinuouslyCompoundedReturns,
-  getSimpleReturns,
-} = require('./src/returns');
+const returns = require('./src/returns');
+const statistics = require('./src/statistics');
 
 module.exports = {
-  getContinuouslyCompoundedReturns,
-  getSimpleReturns,
+  ...returns,
+  ...statistics,
 };
